ea083f601a83cd5f710563bd1235af76bd06f840
danukaem <danukaem@gmail.com>
2025-05-18 17:31:50 +0530

    first commit

---
+++ src/main/java/com/assignment/EventManagementsystem/EventManagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.EventManagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/EventManagementsystem/EventManagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.EventManagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
