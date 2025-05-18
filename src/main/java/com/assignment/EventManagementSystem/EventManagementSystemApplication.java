5a0f786bf2da9506a0676fbbb169309ce7cc106d
danukaem <danukaem@gmail.com>
2025-05-18 17:40:57 +0530

    rename project

--- src/main/java/com/assignment/EventManagementSystem/EventManagementSystemApplication.java
+++ src/main/java/com/assignment/EventManagementSystem/EventManagementSystemApplication.java
@@ -1,13 +1,13 @@
-package com.assignment.EventManagementsystem;
+package com.assignment.EventManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventManagementsystemApplication {
+public class EventManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventManagementsystemApplication.class, args);
+		SpringApplication.run(EventManagementSystemApplication.class, args);
 	}
 
 }
--- src/test/java/com/assignment/EventManagementSystem/EventManagementSystemApplicationTests.java
+++ src/test/java/com/assignment/EventManagementSystem/EventManagementSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.assignment.EventManagementsystem;
+package com.assignment.EventManagementSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventManagementsystemApplicationTests {
+class EventManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
